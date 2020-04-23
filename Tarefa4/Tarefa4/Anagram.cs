392a87c1bd6286b842859ff8c6bb6179a06a9146
NunoAlex221 <48693675+NunoAlex221@users.noreply.github.com>
2020-04-23 19:57:01 +0100

    Tarefa4

---
+++ Tarefa4/Tarefa4/Anagram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace Tarefa4
+{
+    public class Anagram
+    {
+       public static bool IsAnagram(string word1, string word2)
+        {
+            if (word1.Length != word2.Length) return false;
+
+            var word1_ = word1.ToLower().ToCharArray();
+            var word2_ = word2.ToLower().ToCharArray();
+
+            Array.Sort(word1_);
+            Array.Sort(word2_);
+
+            word1 = new string(word1_);
+            word2 = new string(word2_);
+
+            return word1 == word2;
+        }
+    }
+}
---
+++ Tarefa4/Tarefa4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tarefa4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Word1: ");
+            string word1 = Console.ReadLine();
+
+            Console.WriteLine("Word2: ");
+            string word2 = Console.ReadLine();
+
+            Console.WriteLine($"Anagram true or false? {Anagram.IsAnagram(word1, word2)}");
+        }
+    }
+}
