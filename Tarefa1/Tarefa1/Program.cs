533a973096aa1f2457d2b713ad986c6a9125214e
NunoAlex221 <48693675+NunoAlex221@users.noreply.github.com>
2020-04-23 19:53:01 +0100

    Tarefa 1

---
+++ Tarefa1/Tarefa1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tarefa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetFullPath("lyrics.txt");
+            string lyrics = File.ReadAllText(path);
+            
+
+            string[] palavras = lyrics.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> PalavrasRepetidas = new Dictionary<string, int>();
+            for (int i = 0; i < palavras.Length; i++)
+            {
+                if (PalavrasRepetidas.ContainsKey(palavras[i]))
+                {
+                    int value = PalavrasRepetidas[palavras[i]];
+                    PalavrasRepetidas[palavras[i]] += 1;
+                }
+                else
+                {
+                    PalavrasRepetidas.Add(palavras[i], 1);   
+                }
+            }
+            foreach (KeyValuePair<string, int> kvp in PalavrasRepetidas)
+            {
+                Console.WriteLine($"{kvp.Key} repete {kvp.Value} vezes");
+            }
+        }
+    }
+    
+}
