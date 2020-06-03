764803f8c034cd3ca2b27a7c2904c93e00648366
NunoAlex221 <48693675+NunoAlex221@users.noreply.github.com>
2020-06-03 10:05:57 +0100

    Add files via upload

---
+++ exercicio_1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE player(
+   id_PLAYER INTEGER PRIMARY KEY AUTOINCREMENT,
+   name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE type_item(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type VARCHAR (60) NOT NULL
+);
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name_item VARCHAR(60) NOT NULL,
+    quantity INTEGER NULL,
+    type_item VARCHAR(60) NOT NULL,
+    id_player INTEGER NOT NULL,
+    FOREIGN KEY (id_player) REFERENCES player(id_player),
+    FOREIGN KEY (type_item) REFERENCES type_item(id)
+);
